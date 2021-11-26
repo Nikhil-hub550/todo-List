067bfca42b0a300057b9041ebb189308110f6b6a
Nikhil <nikhil1889.cse18@chitkara.edu.in>
2021-11-26 11:29:42 +0530

    we add event emitter, event handling, add forms in todo

--- src/app/MyComponents/todos-item/todos-item.component.ts
+++ src/app/MyComponents/todos-item/todos-item.component.ts
@@ -1,6 +1,7 @@
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from 'src/app/Todo';
+
 
 @Component({
   selector: 'app-todos-item',
@@ -9,9 +10,20 @@ import { Todo } from 'src/app/Todo';
 })
 export class TodosItemComponent implements OnInit {
   @Input() "todo": Todo;
+  @Input() 'i':number;
+  @Output() "todoDelete": EventEmitter<Todo> = new EventEmitter();
+  @Output() "todoCheckbox": EventEmitter<Todo> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onClick(todo: Todo){
+    this.todoDelete.emit(todo);
+    console.log("onclick has been triggerd");
+  }
+  onCheckboxClick(todo:Todo){
+    console.log(todo);
+    this.todoCheckbox.emit(todo);
+    //console.log(todo);
+  }
 }
--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,37 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from "../../Todo";
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
-  todos:Todo[];
-  constructor() {
-    this.todos = [
+  "todos":Todo[];
+  localsetItem: string | null;
+  constructor() { 
+  this.localsetItem=localStorage.getItem("todos")
+  if(this.localsetItem == null)
+  {
+    this.todos  = [];
+  }
+  else
       {
-        sno:1,
-        title: "This is title",
-        desc: "Description",
-        active: true
-      }, 
-       {
-        sno:2,
-        title: "This is title2",
-        desc: "Description",
-        active: true
-      },
-      {
-        sno:3,
-        title: "This is title3",
-        desc: "Description",
-        active: true
-      },
-    ]
+    this.todos=JSON.parse(this.localsetItem);
+      }
    }
-
   ngOnInit(): void {
   }
-
+deleteTodo(todo:Todo){
+  console.log(todo);
+  const index = this.todos.indexOf(todo);
+  this.todos.splice(index, 1);
+  localStorage.setItem("todos", JSON.stringify(this.todos));
+}
+addTodo(todo:Todo){
+  console.log(todo);
+  this.todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(this.todos));
+}
+toggleTodo(todo:Todo){
+  const index = this.todos.indexOf(todo);
+  this.todos[index].active = ! this.todos[index].active;
+  localStorage.setItem("todos", JSON.stringify(this.todos));
+}
 }
