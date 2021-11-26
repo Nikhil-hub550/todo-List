7207377031892fce64a5f40bca4d9ea263e4ef81
Nikhil <nikhil1889.cse18@chitkara.edu.in>
2021-11-26 12:05:36 +0530

    routing, create about page, deploy our project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { TodosComponent } from './MyComponents/todos/todos.component';
 import { TodosItemComponent } from './MyComponents/todos-item/todos-item.component';
 import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './MyComponents/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodosItemComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
